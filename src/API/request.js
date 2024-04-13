6a0b3a5a8a1a8d73fedf6b59ba768296e09dd174
Motaz Ramadan <120125123+motazramadan8@users.noreply.github.com>
2024-04-13 17:33:18 +0200

    Update BaseURL to render

--- src/API/request.js
+++ src/API/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://nodejs-altwheed-portfolio-api-v1-1.onrender.com",
 });
 
 export default request;
